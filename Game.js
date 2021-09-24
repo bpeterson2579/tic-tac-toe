dbaa45d98d6f42ecf49f418572ac0f66faf1e605
Brian Peterson <bpeterson1579@gmail.com>
2021-09-24 10:31:12 -0600

    Add bones to classes and main.js

---
+++ Game.js
@@ -0,0 +1,8 @@
+class Game {
+  constructor() {
+    this.state = [];
+    this.turn = player1;
+    this.win = false;
+  }
+
+}
---
+++ Player.js
@@ -0,0 +1,13 @@
+class Player {
+  constructor() {
+    this.wins = [];
+  }
+
+  saveToStorage() {
+    
+  }
+
+  retrieveFromStorage() {
+
+  }
+}
